626c257fd0b68bf1ece227e1ad0e18f33fe877c5
Mark Hoja <mark.hoja@web.de>
2018-07-29 21:31:59 +0200

    update

--- src/main/java/com/bakerbeach/market/core/service/MessageDaoImpl.java
+++ src/main/java/com/bakerbeach/market/core/service/MessageDaoImpl.java
@@ -7,147 +7,137 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.bakerbeach.market.translation.api.model.I18NMessage;
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
-import com.mongodb.QueryBuilder;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 public class MessageDaoImpl implements MessageDao {
 
-	private MongoTemplate mongoTemplate;
-	private String messagesCollection;
-	private DBCollection dBCollection;
+    private MongoTemplate mongoTemplate;
+    private String messagesCollection;
+    private MongoCollection<Document> dBCollection;
 
-	@Override
-	public void save(I18NMessage message) {
-		String code = message.getKey();
-		String tag = message.getTag();
-		String type = message.getType();
-		
-		DBObject query = new BasicDBObject();
-		query.put("code", code);
-		query.put("tag", tag);
-		query.put("type", type);
-		
-		DBObject update = new BasicDBObject();
-		
-		DBObject translations = new BasicDBObject();
-		for (String locale : message.getAviableLocales()) {
-			translations.put(locale, message.getText(locale));
-		}
-		if (!translations.keySet().isEmpty()) {
-			update.put("$set", new BasicDBObject("translations", translations));
-			getMessagesCollection().findAndModify(query, null, null, false, update, false, true);			
-		}
-	}
-	
-	@Override
-	public I18NMessage findByCode(String tag, String type, String code) throws DAOException {
-		QueryBuilder qb = new QueryBuilder();
-		qb.and("code").is(code);
-		if (tag != null) {
-			qb.and("tag").is(tag);
-		}
-		if (type != null) {
-			qb.and("type").is(type);
-		}
-		DBObject dbo = getMessagesCollection().findOne(qb.get());
-		if (dbo != null) {
-			return convertDBObject2Translation(dbo);
-		} else {
-			throw new DAOException("");
-		}
-	}
+    @Override
+    public void save(I18NMessage message) {
+        String code = message.getKey();
+        String tag = message.getTag();
+        String type = message.getType();
 
-	@Override
-	public List<I18NMessage> findByCodePattern(String tag, String codePattern) {
-		QueryBuilder qb = new QueryBuilder();
-		qb.and("code").regex(Pattern.compile(codePattern));
-		if (tag != null) {
-			qb.and("tag").is(tag);
-		}
-		DBCursor dbc = getMessagesCollection().find(qb.get());
-		List<I18NMessage> result = new ArrayList<I18NMessage>();
-		for (DBObject dbo : dbc) {
-			try {
-				result.add(convertDBObject2Translation(dbo));
-			} catch (Exception e) {
-			}
-		}
-		return result;
-	}
+        Bson filter = Filters.and(Filters.eq("code", code), Filters.eq("tag", tag), Filters.eq("type", type));
 
-	@Override
-	public I18NMessage findReverseTranslation(List<String> tag, String type, String translation, Locale locale)
-			throws DAOException {
-		String key = new StringBuilder("translations.").append(locale).toString();
+        DBObject translations = new BasicDBObject();
+        for (String locale : message.getAviableLocales()) {
+            translations.put(locale, message.getText(locale));
+        }
+        if (!translations.keySet().isEmpty()) {
+            getMessagesCollection().findOneAndUpdate(filter, new Document(new BasicDBObject("translations", translations)));
+        }
+    }
 
-		QueryBuilder qb = new QueryBuilder();
-		qb.and(key).is(translation);
-		qb.and("tag").in(tag);
-		qb.and("type").is(type);
-		DBObject dbo = getMessagesCollection().findOne(qb.get());
-		if (dbo != null) {
-			I18NMessageImpl message = new I18NMessageImpl();
-			message.setMessageKey(translation);
-			message.setTag((String) dbo.get("tag"));
-			message.setType((String) dbo.get("type"));
+    @Override
+    public I18NMessage findByCode(String tag, String type, String code) throws DAOException {
+        Bson filter = Filters.eq("code", code);
+        if (tag != null) {
+            filter = Filters.and(filter, Filters.eq("tag", tag));
+        }
+        if (type != null) {
+            filter = Filters.and(filter, Filters.eq("type", type));
+        }
+        FindIterable<Document> dbo = getMessagesCollection().find(filter);
+        if (dbo != null && dbo.first() != null) {
+            return convertDBObject2Translation(dbo.first());
+        } else {
+            throw new DAOException("");
+        }
+    }
 
-			HashMap<String, String> messageMap = new HashMap<String, String>();
-			messageMap.put(Locale.ENGLISH.toString(), (String) dbo.get("code"));
-			message.setMessages(messageMap);
+    @Override
+    public List<I18NMessage> findByCodePattern(String tag, String codePattern) {
+        Bson filter = Filters.regex("code", Pattern.compile(codePattern));
+        if (tag != null) {
+            filter = Filters.and(filter, Filters.eq("tag", tag));
+        }
+        FindIterable<Document> dbo = getMessagesCollection().find(filter);
+        List<I18NMessage> result = new ArrayList<>();
+        for (Document document : dbo) {
+            try {
+                result.add(convertDBObject2Translation(document));
+            } catch (Exception e) {
+            }
+        }
+        return result;
+    }
 
-			return message;
-		} else {
-			throw new DAOException("");
-		}
-	}
+    @Override
+    public I18NMessage findReverseTranslation(List<String> tag, String type, String translation, Locale locale) throws DAOException {
+        String key = new StringBuilder("translations.").append(locale).toString();
 
-	public MongoTemplate getMongoTemplate() {
-		return mongoTemplate;
-	}
+        Bson filter = Filters.and(Filters.eq(key, translation), Filters.in("tag", tag), Filters.eq("type", type));
+        FindIterable<Document> dbo = getMessagesCollection().find(filter);
+        if (dbo != null && dbo.first() != null) {
+            I18NMessageImpl message = new I18NMessageImpl();
+            message.setMessageKey(translation);
+            message.setTag((String) dbo.first().get("tag"));
+            message.setType((String) dbo.first().get("type"));
 
-	public void setMongoTemplate(MongoTemplate mongoTemplate) {
-		this.mongoTemplate = mongoTemplate;
-	}
+            HashMap<String, String> messageMap = new HashMap<>();
+            messageMap.put(Locale.ENGLISH.toString(), (String) dbo.first().get("code"));
+            message.setMessages(messageMap);
 
-	private DBCollection getMessagesCollection() {
-		if (dBCollection == null)
-			dBCollection = mongoTemplate.getCollection(messagesCollection);
-		return dBCollection;
-	}
+            return message;
+        } else {
+            throw new DAOException("");
+        }
+    }
 
-	private I18NMessage convertDBObject2Translation(DBObject dbo) {
-		I18NMessageImpl translation = new I18NMessageImpl();
-		translation.setMessageKey((String) dbo.get("code"));
-		translation.setTag((String) dbo.get("tag"));
-		translation.setType((String) dbo.get("type"));
-		HashMap<String, String> messageMap = new HashMap<String, String>();
-		@SuppressWarnings("unchecked")
-		Map<String, String> translations = (Map<String, String>) dbo.get("translations");
-		if (translations != null) {
-			for (Map.Entry<String, String> entry : translations.entrySet()) {
-				String lang = entry.getKey();
-				Locale locale;
-				if (lang.contains("_")) {
-					locale = new Locale(lang.split("_")[0], lang.split("_")[1]);
-				} else {
-					locale = new Locale(lang);
-				}
-				String msg = entry.getValue();
-				messageMap.put(locale.toString(), msg);
-			}
-			translation.setMessages(messageMap);
-		}
-		return translation;
-	}
+    public MongoTemplate getMongoTemplate() {
+        return mongoTemplate;
+    }
 
-	public void setMessagesCollection(String messagesCollection) {
-		this.messagesCollection = messagesCollection;
-	}
+    public void setMongoTemplate(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    private MongoCollection<Document> getMessagesCollection() {
+        if (dBCollection == null)
+            dBCollection = mongoTemplate.getCollection(messagesCollection);
+        return dBCollection;
+    }
+
+    private I18NMessage convertDBObject2Translation(Map<?, ?> dbo) {
+        I18NMessageImpl translation = new I18NMessageImpl();
+        translation.setMessageKey((String) dbo.get("code"));
+        translation.setTag((String) dbo.get("tag"));
+        translation.setType((String) dbo.get("type"));
+        HashMap<String, String> messageMap = new HashMap<>();
+        @SuppressWarnings("unchecked")
+        Map<String, String> translations = (Map<String, String>) dbo.get("translations");
+        if (translations != null) {
+            for (Map.Entry<String, String> entry : translations.entrySet()) {
+                String lang = entry.getKey();
+                Locale locale;
+                if (lang.contains("_")) {
+                    locale = new Locale(lang.split("_")[0], lang.split("_")[1]);
+                } else {
+                    locale = new Locale(lang);
+                }
+                String msg = entry.getValue();
+                messageMap.put(locale.toString(), msg);
+            }
+            translation.setMessages(messageMap);
+        }
+        return translation;
+    }
+
+    public void setMessagesCollection(String messagesCollection) {
+        this.messagesCollection = messagesCollection;
+    }
 
 }
--- src/main/java/com/bakerbeach/market/translation/handler/TranslationCacheHandler.java
+++ src/main/java/com/bakerbeach/market/translation/handler/TranslationCacheHandler.java
@@ -7,60 +7,60 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bakerbeach.market.translation.api.service.TranslationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TranslationCacheHandler {
-	final Logger log = LoggerFactory.getLogger(TranslationCacheHandler.class);
+    final Logger log = LoggerFactory.getLogger(TranslationCacheHandler.class);
 
-	@Autowired
-	private TranslationService translationService;
+    @Autowired
+    private TranslationService translationService;
 
-	public void clearCache(Exchange ex) {
-		try {
-			Map<String, Object> payload = getPayload(ex.getIn());
-			if (payload != null) {
-				String tag = (String) payload.get("tag");
-				String type = (String) payload.get("type");
-				String code = (String) payload.get("code");
-				
-				if (StringUtils.isNotBlank(tag) && StringUtils.isNotBlank(type) && StringUtils.isNotBlank(code)) {
-					translationService.clearCache(tag, type, code);					
-				} else {
-					translationService.clearCache();					
-				}				
-			} else {
-				translationService.clearCache();				
-			}
-		} catch (Exception e) {
-			translationService.clearCache();				
-			log.error(ExceptionUtils.getStackTrace(e));
-		}
-	}
+    public void clearCache(Exchange ex) {
+        try {
+            Map<String, Object> payload = getPayload(ex.getIn());
+            if (payload != null) {
+                String tag = (String) payload.get("tag");
+                String type = (String) payload.get("type");
+                String code = (String) payload.get("code");
 
-	@SuppressWarnings("unchecked")
-	protected Map<String, Object> getPayload(Message message) throws Exception {
-		Map<String, Object> payload = null;
-		try {			
-			if (message.getBody() instanceof String) {
-				String payloadAsString = (String) message.getBody();
-				if (payloadAsString != null && !payloadAsString.isEmpty()) {
-					ObjectMapper mapper = new ObjectMapper();
-					Map<String, Object> map = (Map<String, Object>) mapper.readValue(payloadAsString, Map.class);
-					message.setBody(map);
-				}
-			}
-						
-			payload = (Map<String, Object>) message.getBody();
-		} catch (IOException e) {
-			log.error(ExceptionUtils.getStackTrace(e));
-		}
-		
-		return payload;
-	}
+                if (StringUtils.isNotBlank(tag) && StringUtils.isNotBlank(type) && StringUtils.isNotBlank(code)) {
+                    translationService.clearCache(tag, type, code);
+                } else {
+                    translationService.clearCache();
+                }
+            } else {
+                translationService.clearCache();
+            }
+        } catch (Exception e) {
+            translationService.clearCache();
+            log.error(ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected Map<String, Object> getPayload(Message message) throws Exception {
+        Map<String, Object> payload = null;
+        try {
+            if (message.getBody() instanceof String) {
+                String payloadAsString = (String) message.getBody();
+                if (payloadAsString != null && !payloadAsString.isEmpty()) {
+                    ObjectMapper mapper = new ObjectMapper();
+                    Map<String, Object> map = mapper.readValue(payloadAsString, Map.class);
+                    message.setBody(map);
+                }
+            }
+
+            payload = (Map<String, Object>) message.getBody();
+        } catch (IOException e) {
+            log.error(ExceptionUtils.getStackTrace(e));
+        }
+
+        return payload;
+    }
 
 }
